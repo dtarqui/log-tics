ab0139e1f0b12578bbc6b05753bf1d6472ccf4a5
Fulano2141 <macuto2142dt@gmail.com>
2020-10-06 14:34:23 -0400

    tabla y eso

---
+++ src/components/NewRow.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+      New Item
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
